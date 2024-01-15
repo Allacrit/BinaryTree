43e262725e74dcdd108de21f70ead6e0c9676859
Artem Oleynikov <allacrit@yandex.ru>
2024-01-15 23:00:20 +0300

    end

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        BinaryTree<Integer> tree = new BinaryTree<>();
+
+        tree.add(5);
+        tree.add(2);
+        tree.add(1);
+        tree.add(3);
+        tree.add(4);
+        tree.add(30);
+        tree.add(8);
+        tree.add(6);
+        tree.add(10);
+        tree.add(19);
+        tree.add(21);
+        tree.add(51);
+        tree.add(100);
+
+        tree.print();
     }
 }
